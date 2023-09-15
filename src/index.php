5a9d66148aaa89baa08c375fc84619016d9596bc
André Nascimento <andre.neospace@gmail.com>
2023-09-15 19:07:43 -0300

    base index structure

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<?php
+define('BASE', __DIR__);
+define('URL', "http://localhost:8100");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cryptus.io</title>
+    <link rel="stylesheet" href="<?= URL . '/assets/css/' ?>style.css">
+    <link rel="stylesheet" href="<?= URL . '/assets/css/' ?>lunar.min.css">
+</head>
+<body>
+    
+    <script src="<?= URL . '/assets/js/lunar.min.js' ?>"></script>
+    <script src="<?= URL . '/assets/js/script.js' ?>"></script>
+</body>
+</html>
