c57020acbc4dba9547cadf9aa17f31520a2ebb7a
André Nascimento <andre.neospace@gmail.com>
2023-09-16 15:28:44 -0300

    nav backdrop

--- src/partials/navbar.php
+++ src/partials/navbar.php
@@ -11,7 +11,7 @@
                 <span class="hidden md:block">Become a reseller</span>
                 <span class="md:hidden">Start Now</span>
             </button>
-            <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+            <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700/30 focus:ring-white/30 duration-500" aria-controls="navbar-sticky" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
@@ -19,7 +19,7 @@
             </button>
         </div>
         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-            <ul class="bg-white/20 md:bg-transparent flex flex-col p-4 md:p-0 mt-4 font-medium rounded-lg md:flex-row md:space-x-8 md:mt-0">
+            <ul class="backdrop-blur bg-white/10 md:bg-transparent flex flex-col p-4 md:p-0 mt-4 font-medium rounded-lg md:flex-row md:space-x-8 md:mt-0">
                 <li>
                     <a href="#" class="nav-item">Products</a>
                 </li>
